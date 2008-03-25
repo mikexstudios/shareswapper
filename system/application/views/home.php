94204619a7733683585dbc4731bc63258c02975d
Michael Huynh <mike.huynh@gmail.com>
2008-03-25 07:03:47 +0000

    Well, decided on CodeIgniter since I wanted stability. Added the home page.

---
+++ system/application/views/home.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Share Swapper - find files from online file hosting providers</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+	<style type="text/css" media="screen">
+	/* <![CDATA[ */
+	@import url("css/common.css");
+	@import url("css/home.css");
+	/* ]]> */
+	</style>
+	
+	<!-- <link rel="shortcut icon" href="favicon.ico" /> -->
+</head>
+
+<body>
+<div id="wrap">
+
+<div id="header">
+	<h1><a href="http://www.shareswapper.com">Share Swapper</a></h1>
+	<p>a large index of <strong>user submitted and verified files</strong> 
+	from online file hosting providers that include <span class="highlight">rapidshare, 
+	megaupload, mediafire, sendspace, and more</span>!</p>
+</div>
+
+<div id="content">
+	
+	<div class="navigation">
+		<p><strong>Search Files</strong> | <a href="browse">Browse Files</a> | 
+		<a href="recent">Most Recent Files</a> | <a href="add">Submit a File Link!</a></p>  
+	</div>
+	
+	<div class="search">
+		<form action="search" method="GET">
+			<input type="text" name="keywords" class="searchbox" />
+			<input type="submit" value="Search!" class="searchsubmit" />
+		</form>
+		<!-- TODO: Add checkboxes for categories -->
+	</div>
+	
+	<div class="usefullinks">
+		<p><a href="add"><span class="highlight">Have a file link to share? Submit it here!</span></a></p>
+	</div>
+	
+	<!-- TODO: Add list of recently added files or recently access files or recent searches -->
+	
+</div>
+
+<div id="footer">
+  <p><a href="about">about</a> | <a href="contact">contact</a> | <a href="copyright">copyright policy</a> | {elapsed_time} seconds</p>
+</div>
+
+</div> <!-- End div for wrap -->
+
+</body>
+
+</html>
