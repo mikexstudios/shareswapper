8b17ca4c35dc73a1ac460f891a6812097a433234
Michael Huynh <mike.huynh@gmail.com>
2008-05-11 22:37:33 +0000

    Added Google Analytics tracking code to page-footer.php

--- application/views/page-footer.php
+++ application/views/page-footer.php
@@ -2,3 +2,13 @@
 <div id="footer">
   <p><a href="about">about</a> | <a href="contact">contact</a> | <a href="copyright">copyright policy</a> | {execution_time} seconds</p>
 </div>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-56433-2");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
